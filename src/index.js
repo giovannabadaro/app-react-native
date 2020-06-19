e5b5ae7cfc79219adb576cc0fa3eca1c32301349
Giovanna <giovannabadaroalves@gmail.com>
2020-06-19 18:59:29 -0300

    add button + POST

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React,{ useState, useEffect} from 'react';
-import { View, Text, StyleSheet, StatusBar, Button} from 'react-native'
+import { View, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity} from 'react-native'
 // import { Container } from './styles';
 import api from '../src/services/api'
 // todos o componentes vem com Display: flex
@@ -15,14 +15,47 @@ function App() {
     })
   },[])
 
+  async function handleAddProject(){
+    const response = await api.post('projects', {
+      title: `"Novo Projeto ${Date.now()}"`,
+      owner: 'Giovanna'
+    });
+
+    setProjects([...projects, response.data]);
+  }
+
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="blue"/>
-      <View style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor="purple"/>
+
+      <FlatList 
+        style={styles.container}
+        data={projects}
+        keyExtractor={project => project.id}
+        renderItem={({ item: project })=>( //convertendo uma variável item em uma variável project 
+          <Text style={styles.container}>
+            {project.title}
+          </Text>
+        )} 
+      />
+
+     <TouchableOpacity 
+      style={styles.button}
+      activeOpacity={0.8}
+      onPress={handleAddProject}
+      >
+
+       <Text style={styles.buttonText}>
+         Adicionar um projeto
+       </Text>
+
+     </TouchableOpacity>
+
+      {/* <View style={styles.container}>
         {projects.map(project => 
           <Text key={project.id}>{project.title}</Text>
           ) }
-      </View>
+      </View> */}
     </>
   );
 
@@ -31,15 +64,29 @@ function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: "#bdbdbd"
+    flex: 1,   
+    backgroundColor: "#eee",
+    fontWeight: "bold"
   },
   title: {
     fontSize: 16,
     color: '#fff',
     fontWeight: 'bold'
+  },
+  button:{
+    backgroundColor: 'purple',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems:'center'
+
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: '#fff'
+
   }
 });
 
